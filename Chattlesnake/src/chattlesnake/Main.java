515192f5b9d33c08d9c03ac72b1fa5e9b94d54fc
Cameron Justice <cameronjustice@protonmail.com>
2019-09-03 13:13:43 -0500

    Added java project directory with boilerplate code.

---
+++ Chattlesnake/src/chattlesnake/Main.java
@@ -0,0 +1,24 @@
+package chattlesnake;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StartUp();
+		ShutDown();
+	}
+	
+	public static void StartUp() {
+		// Startup code
+		
+		System.out.println("Chattlesnake up and running.");
+	}
+	
+	public static void ShutDown() {
+		System.out.println("Chattlesnake shutting down...");
+		
+		// Shutdown code
+		
+		System.out.println("Chattlesnake shut down.");
+	}
+
+}
