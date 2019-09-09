2ebf1831ce6ae195651666cbef4714a47a11a5f2
Chris Gerspacher <cag7a@mtmail.mtsu.edu>
2019-09-09 18:38:03 -0500

    Added a Fullscreen Toggle under the Help Menu section
    Fixed the "escape to exit fullscreen" bug

--- Chattlesnake/src/main/Main.java
+++ Chattlesnake/src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Chattlesnake");
         primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         primaryStage.show();
     }
 
