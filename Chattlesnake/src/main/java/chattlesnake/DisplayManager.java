e189893221794ca956f9b0a3de18ca7901196548
Chris Gerspacher <cag7a@mtmail.mtsu.edu>
2019-09-16 17:28:22 -0500

    Updated the showMessage to display a message to the terminal

--- Chattlesnake/src/main/java/chattlesnake/DisplayManager.java
+++ Chattlesnake/src/main/java/chattlesnake/DisplayManager.java
@@ -3,8 +3,15 @@ package chattlesnake;
 public class DisplayManager {
 
     public void showMessage(Message msg) {
+        int author_id = msg.getAuthor_id();
+        int recipient_id = msg.getRecipient_id();
+        String message = msg.getMessage_body();
+        String time = msg.getCreate_date().toLocalTime().toString();
 
-
+        System.out.println("From: " + author_id);
+        System.out.println("To: " + recipient_id);
+        System.out.println(message);
+        System.out.println("Message sent at: " + time);
     }
 
     public void showGroup(Group group) {
