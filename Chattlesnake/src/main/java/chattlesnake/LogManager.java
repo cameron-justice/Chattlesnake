76a2a31431c8de4fd17071deb34ea684000e17e5
Nathan Byrnes <ndb3w@mtmail.mtsu.edu>
2019-09-23 16:48:31 -0500

    Writes to JSON object

--- Chattlesnake/src/main/java/chattlesnake/LogManager.java
+++ Chattlesnake/src/main/java/chattlesnake/LogManager.java
@@ -1,13 +1,15 @@
 package chattlesnake;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import org.json.simple.JSONObject;
 import java.time.LocalDateTime;
 
 public class LogManager {
-    public static void log(Message mess) throws FileNotFoundException {
+    public void log(Message mess) throws FileNotFoundException {
         System.out.println("This is the branch section");
+
 
         JSONObject log = new JSONObject();
         int author_id = 0;
@@ -19,6 +21,11 @@ public class LogManager {
         recipient_id = mess.getRecipient_id();
         message_body = mess.getMessage_body();
         create_date = mess.getCreate_date();
+
+        log.put("Author: ", author_id);
+        log.put("Recipient: ", recipient_id);
+        log.put("Message Body: ", message_body);
+        log.put("Date: ", create_date);
 
         PrintWriter pw = new PrintWriter("Log.txt");
         pw.write(log.toJSONString());
